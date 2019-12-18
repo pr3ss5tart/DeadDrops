8e586ddf00c3a7132e3dd8fb725d0a19d50855b9
Ben Loesch <benloesch@gmail.com>
2019-12-17 21:09:23 -0800

    Initial DeadDrops commit. Tomorrow I'll work on getting the clues panel working and scrolling. I'll also need to finish the rest of the UI.

---
+++ Assets/Scripts/Code.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Code : MonoBehaviour
+{
+    KeyboardScript kbs;
+    public string bombCode;
+    public string guess;
+    public int guessesLeft=3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        kbs = GetComponent<KeyboardScript>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Get_Code()
+    {
+        bombCode = kbs.code;
+    }
+
+    public void Check_Code()
+    {
+        if((guess == bombCode) && (guessesLeft > 0))
+        {
+            //Stop timer and defuse nuke
+        }else if (guessesLeft > 0)
+        {
+            guessesLeft--;
+        }
+        else
+        {
+            //End game, show nuke effect
+        }
+    }
+}
